16bcb835ac929b5e7d81223f9929be388bbb64fd
Aryaabhisek Mahapatra <aryaabhisek661@gmail.com>
2026-05-01 20:43:40 +0530

    fix: separate Query.js and axios.js into correct files

--- server/models/Query.js
+++ server/models/Query.js
@@ -12,18 +12,3 @@ const QuerySchema = new mongoose.Schema({
 });
 
 module.exports = mongoose.model('Query', QuerySchema);
-
-// client/src/api/axios.js
-import axios from 'axios';
-
-const baseURL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
-
-const api = axios.create({ baseURL });
-
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
-  if (token) config.headers.Authorization = `Bearer ${token}`;
-  return config;
-});
-
-export default api;
