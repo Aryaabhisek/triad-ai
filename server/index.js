bf4878bf793e3a10b52d303bafb163d984550103
Aryaabhisek Mahapatra <aryaabhisek661@gmail.com>
2026-05-02 07:33:44 +0530

    fix: handle trailing slash in CORS origin check

--- server/index.js
+++ server/index.js
@@ -8,6 +8,8 @@ const app = express();
 const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:3000',
+  'https://triadai.vercel.app',
+  'https://triadai.vercel.app/',
   process.env.CLIENT_URL || 'http://localhost:5173'
 ];
 
